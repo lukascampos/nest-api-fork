84b58a9bf63e9bcde959833252374cb8b9ec25dc
RAFAEL GRACILIANO DE BARROS <CG3023613@car.ifsp>
2025-04-15 21:43:05 -0300

    verificação de role

--- src/domain/identity/user/list-user/list-user.controller.ts
+++ src/domain/identity/user/list-user/list-user.controller.ts
@@ -1,12 +1,19 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Controller, Get, ForbiddenException, Request} from '@nestjs/common';
 import { ListUserService } from './list-user.service';
+
 
 @Controller('users')
 export class ListUserController {
   constructor(private readonly listUserService: ListUserService) {}
 
   @Get()
-  async findAll() {
+  async findAll(@Request() req) {
+    const currentUser = req.user;
+
+    if (currentUser.role !== 'administrador') {
+      throw new ForbiddenException('Apenas Administradores podem acessar esta rota');
+    }
+    
     return this.listUserService.findAll();
   }
 }
