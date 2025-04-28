ac13cbf436292a887b7a5bb0aaa383a0a621200a
Rafael Barros <rgracilianobarros@gmail.com>
2025-04-28 09:23:25 -0300

    logicas no metodo de listagem - somente ativos e filtro por role

--- src/domain/identity/user/list-user/list-user.service.ts
+++ src/domain/identity/user/list-user/list-user.service.ts
@@ -11,17 +11,17 @@ interface User {
 export class ListUserService {
 
   constructor(
-    private readonly prisma: PrismaService // Injetando o PrismaService para acessar o banco de dados
+    private readonly prisma: PrismaService // injecao o PrismaService para acesso ao banco
   ){}
-
 
   async findAll(requestingUser: User) {
     console.log('Prisma:', this.prisma);
-    const { id, role } = requestingUser; // Desestruturacao do objeto "Usuario"
+    const { id, role } = requestingUser; // Desestruturacao do objeto "Usuario" recebido
     if (role === Role.ADMIN) {
       console.log('Admin acessando todos os usuários');
       // Admin pode ver todos os usuários
       return this.prisma.user.findMany({
+        where: {isDisabled: false}, // Adicionando filtro para usuários não desativados
         include: { profile: true, artisan: true },
       });
     }
@@ -29,16 +29,26 @@ export class ListUserService {
     if (role === Role.MODERATOR) {
       // Moderador vê apenas "Usuarios"
       return this.prisma.user.findMany({
-        where: { role: Role.USER },
+        where: { 
+          role: Role.USER,
+          isDisabled: false,
+         },
         include: { profile: true, artisan: true },
       });
     }
 
-    if (role === Role.USER || role === Role.ARTISAN) {  // Usuários e artesãos veem apenas os próprios dados
-      return this.prisma.user.findUnique({
+    if (role === Role.USER || role === Role.ARTISAN) {
+      // Usuários e artesao veem apenas os próprios dados
+      const user = await this.prisma.user.findUnique({
         where: { id },
         include: { profile: true, artisan: true },
       });
+  
+      if (!user || user.isDisabled) {
+        throw new UnauthorizedException('Usuário desativado ou não encontrado');
+      } // Garantia de que o usuário existe e não está desativado para listagem
+  
+      return user;
     }
 
     throw new UnauthorizedException('Acesso negado');
