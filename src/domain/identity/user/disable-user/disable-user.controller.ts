a165b1744cbc195a11bcff0a6beef649d1428ce2
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2025-04-27 21:16:20 -0300

    feat: Modification of the function to create a user with the ADMIN role and implementation of the controller to deactivate users.

--- src/domain/identity/user/disable-user/disable-user.controller.ts
+++ src/domain/identity/user/disable-user/disable-user.controller.ts
@@ -1,11 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Patch, Body } from '@nestjs/common';
 import { JwtAuthGuard } from '@/domain/auth/jwt-auth.guard';
-import { Roles } from '@/domain/auth/roles/roles.decorator';
 import { RolesGuard } from '@/domain/auth/roles/roles.guard';
+import { CurrentUser } from '@/domain/auth/current-user.decorator';
+import { UserPayload } from '@/domain/auth/jwt.strategy';
+import { DisableUserService } from './disable-user.service';
+import { DisableUserDto } from './disable-user.dto';
 
-@Controller('disable-user')
+@Controller('/disable-user')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class DisableUserController {
+ constructor( private readonly disableUserService: DisableUserService ){}
 
+  @Patch()
+  async handle(
+    @Body() dto: DisableUserDto,
+    @CurrentUser() userId: UserPayload,
+    ) {
+    return this.disableUserService.disableUser(dto, userId);
+    }
 }
-export default DisableUserController;
--- src/domain/identity/user/user.module.ts
+++ src/domain/identity/user/user.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CreateUserModule } from './create-user/create-user.module';
 import { UpdateUserProfileInfoModule } from './update-user-profile-info/update-user-profile-info.module';
+import { DisableUserModule } from './disable-user/disable-user.module';
 
 @Module({
-  imports: [CreateUserModule, UpdateUserProfileInfoModule],
+  imports: [CreateUserModule, UpdateUserProfileInfoModule, DisableUserModule],
 })
 export class UserModule {}
