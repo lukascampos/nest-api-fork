908f0fa9668e92734397fa25ef068352d5c5146a
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2025-04-29 22:18:59 -0300

    feat: enhance authentication response to include user profile information

--- src/domain/auth/authenticate/authenticate.controller.ts
+++ src/domain/auth/authenticate/authenticate.controller.ts
@@ -1,6 +1,8 @@
 import {
   Body, Controller, Post,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthenticateService } from './authenticate.service';
 import { AuthenticateDto } from './authenticate.dto';
 import { Public } from '../public.decorator';
@@ -13,7 +15,20 @@ export class AuthenticateController {
 
   @Post()
   @Public()
-  async handle(@Body() body: AuthenticateDto) {
-    return this.authenticateService.execute(body);
+  async handle(@Body() body: AuthenticateDto, @Res() res: Response) {
+    const {
+      accessToken, role, avatar, name,
+    } = await this.authenticateService.execute(body);
+
+    res.cookie('access_token', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    return res.send({
+      role, name, avatar,
+    });
   }
 }
