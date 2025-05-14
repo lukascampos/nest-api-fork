6b2b2920ee3f5e4d0b71a2b5a60a95a4654bb6a4
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2025-05-14 11:19:39 -0300

    refactor: remove CreateUser and RegisterWithArtisan modules and related components

--- src/domain/identity/user/user.module.ts
+++ src/domain/identity/user/user.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { UpdateUserProfileInfoModule } from './update-user-profile-info/update-user-profile-info.module';
 import { ListUserModule } from './list-user/list-user.module';
 import { DisableUserModule } from './disable-user/disable-user.module';
-import { RegisterWithArtisanModule } from './register-with-artisan/register-with-artisan.module';
 
 @Module({
   imports: [
     UpdateUserProfileInfoModule,
     DisableUserModule,
     ListUserModule,
-    RegisterWithArtisanModule,
   ],
 })
 export class UserModule {}
