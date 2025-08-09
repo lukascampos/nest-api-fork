bfaa318ee32ba6a0ebf553aafa235249cd4b099d
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2025-08-08 21:35:38 -0300

    refactor: clean up code formatting and improve consistency across files

--- src/domain/product/http/http.module.ts
+++ src/domain/product/http/http.module.ts
@@ -1,7 +1,7 @@
-import { Module } from "@nestjs/common";
-import { CreateProductController } from "./controllers/create-product.controller";
-import { ProductPersistenceModule } from "../persistence/product-persistence.module";
-import { CreateProductUseCase } from "../core/use-cases/create-product.use-case";
+import { Module } from '@nestjs/common';
+import { CreateProductController } from './controllers/create-product.controller';
+import { ProductPersistenceModule } from '../persistence/product-persistence.module';
+import { CreateProductUseCase } from '../core/use-cases/create-product.use-case';
 
 @Module({
   imports: [ProductPersistenceModule],
@@ -9,7 +9,7 @@ import { CreateProductUseCase } from "../core/use-cases/create-product.use-case"
     CreateProductController,
   ],
   providers: [
-    CreateProductUseCase
+    CreateProductUseCase,
   ],
 })
 export class HttpModule {}
--- src/domain/product/persistence/prisma/repositories/prisma-product-photos.repository.ts
+++ src/domain/product/persistence/prisma/repositories/prisma-product-photos.repository.ts
@@ -1,7 +1,7 @@
-import { ProductPhoto } from "@/domain/product/core/entities/product-photo.entity"
-import { PrismaService } from "@/shared/prisma/prisma.service"
-import { Injectable } from "@nestjs/common"
-import { PrismaProductPhotosMapper } from "../mappers/prisma-product-photos.mapper"
+import { Injectable } from '@nestjs/common';
+import { ProductPhoto } from '@/domain/product/core/entities/product-photo.entity';
+import { PrismaService } from '@/shared/prisma/prisma.service';
+import { PrismaProductPhotosMapper } from '../mappers/prisma-product-photos.mapper';
 
 @Injectable()
 export class PrismaProductPhotosRepository {
@@ -9,22 +9,20 @@ export class PrismaProductPhotosRepository {
 
   async createMany(photos: ProductPhoto[]): Promise<void> {
     if (photos.length === 0) {
-      return
+      return;
     }
 
     const data = PrismaProductPhotosMapper.toPrismaUpdateMany(photos);
-    
+
     await this.prisma.attachment.updateMany(data);
   }
 
   async deleteMany(attachments: ProductPhoto[]): Promise<void> {
     if (attachments.length === 0) {
-      return
+      return;
     }
 
-    const attachmentIds = attachments.map((attachment) => {
-      return attachment.id.toString()
-    })
+    const attachmentIds = attachments.map((attachment) => attachment.id.toString());
 
     await this.prisma.attachment.deleteMany({
       where: {
@@ -32,7 +30,7 @@ export class PrismaProductPhotosRepository {
           in: attachmentIds,
         },
       },
-    })
+    });
   }
 
   async deleteManyByProductId(productId: string): Promise<void> {
@@ -40,6 +38,6 @@ export class PrismaProductPhotosRepository {
       where: {
         productId,
       },
-    })
+    });
   }
 }
