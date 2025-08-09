4c0aa06283d6da085114fb0f296291da89b36e35
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2025-08-08 22:25:07 -0300

    add find by id method

--- src/domain/_shared/attachments/persistence/prisma/repositories/prisma-attachments.repository.ts
+++ src/domain/_shared/attachments/persistence/prisma/repositories/prisma-attachments.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@/shared/prisma/prisma.service';
 import { Attachment } from '../../../core/entities/attachment.entity';
+import { PrismaAttachmentsMapper } from '../mappers/prisma-attachments.mapper';
 
 @Injectable()
 export class PrismaAttachmentsRepository {
@@ -22,5 +23,19 @@ export class PrismaAttachmentsRepository {
       create: commonData,
       update: commonData,
     });
+  }
+
+  async findById(id: string): Promise<Attachment | null> {
+    const attachment = await this.prisma.attachment.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!attachment) {
+      return null;
+    }
+
+    return PrismaAttachmentsMapper.toDomain(attachment);
   }
 }
