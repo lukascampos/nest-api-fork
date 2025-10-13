b2156a726dc6b287f660537af12830361a04c7ca
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2025-10-13 12:21:28 -0300

    Remove JwtAuthGuard from GetArtisanProfileByUsernameController

--- src/domain/identity/http/controllers/get-artisan-profile-by-username.controller.ts
+++ src/domain/identity/http/controllers/get-artisan-profile-by-username.controller.ts
@@ -2,16 +2,13 @@ import {
   Controller,
   Get,
   Param,
-  UseGuards,
   NotFoundException,
   BadRequestException,
 } from '@nestjs/common';
-import { JwtAuthGuard } from '@/domain/_shared/auth/jwt/jwt-auth.guard';
 import { GetArtisanProfileByUsernameUseCase } from '../../core/use-cases/get-artisan-profile-by-username.use-case';
 import { UserNotFoundError } from '../../core/errors/user-not-found.error';
 
 @Controller('artisan-profiles/:username')
-@UseGuards(JwtAuthGuard)
 export class GetArtisanProfileByUsernameController {
   constructor(
     private readonly getArtisanProfileByUsername: GetArtisanProfileByUsernameUseCase,
