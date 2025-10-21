5132206834596436fb7ec3a33e44befdaca1b97e
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2025-10-21 00:00:07 -0300

    fix: update error message in CannotFollowSelfError to use correct grammar

--- src/domain/identity/core/errors/cannot-follow-self.error.ts
+++ src/domain/identity/core/errors/cannot-follow-self.error.ts
@@ -1,6 +1,6 @@
 export class CannotFollowSelfError extends Error {
   constructor() {
-    super('User cannot follow yourself.');
+    super('User cannot follow themselves.');
     this.name = 'CannotFollowSelfError';
   }
 }
